373d7c96a362499cd7a08444316b8759d79af641
Will Snavely <will.snavely@gmail.com>
2026-01-30 16:40:04 -0500

    fixing class query when there are no sql parameters

--- includes/model/db/queries/class-usctdp-mgmt-clinic-class-query.php
+++ includes/model/db/queries/class-usctdp-mgmt-clinic-class-query.php
@@ -2,7 +2,7 @@
 
 use BerlinDB\Database\Query;
 
-if (! defined('ABSPATH')) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
@@ -51,38 +51,38 @@ class Usctdp_Mgmt_Clinic_Class_Query extends Query
 
         $token_suffix = Usctdp_Mgmt_Model::$token_suffix;
         $query = $wpdb->prepare(
-            "   SELECT 
+            "   SELECT
                     cls.id as class_id, cls.title as class_name, cls.day_of_week as class_day_of_week,
                     cls.start_time as class_start_time, cls.end_time as class_end_time,
                     cls.capacity as class_capacity, cls.level as class_level,
                     cls.notes as class_notes,
-                    sess.id as session_id, 
+                    sess.id as session_id,
                     REPLACE(sess.title, '{$token_suffix}', '') as session_name,
                     sess.start_date as session_start_date, sess.end_date as session_end_date,
                     sess.num_weeks as session_num_weeks, sess.category as session_category,
                     post.post_title as clinic_name, post.ID as clinic_id
-                FROM {$wpdb->prefix}usctdp_clinic_class AS cls 
+                FROM {$wpdb->prefix}usctdp_clinic_class AS cls
                 JOIN {$wpdb->prefix}usctdp_session AS sess ON cls.session_id = sess.id
-                JOIN {$wpdb->prefix}posts AS post ON cls.clinic_id = post.ID 
+                JOIN {$wpdb->prefix}posts AS post ON cls.clinic_id = post.ID
                 {$where_clause}
                 ORDER BY cls.id DESC
                 {$limit_clause}",
             array_merge($where_args, $limit_args)
         );
         $window = $wpdb->get_results($query);
-
-        $count_query = $wpdb->prepare(
-            "   SELECT COUNT(*) as count
-                FROM {$wpdb->prefix}usctdp_clinic_class AS cls 
+        $count_sql = "SELECT COUNT(*) as count
+                FROM {$wpdb->prefix}usctdp_clinic_class AS cls
                 JOIN {$wpdb->prefix}usctdp_session AS sess ON cls.session_id = sess.id
-                JOIN {$wpdb->prefix}posts AS post ON cls.clinic_id = post.ID 
-                {$where_clause}",
-            $where_args
-        );
-        $count = $wpdb->get_row($count_query);
+                JOIN {$wpdb->prefix}posts AS post ON cls.clinic_id = post.ID
+                {$where_clause}";
+        $count_query = $count_sql;
+        if (!empty($where_args)) {
+            $count_query = $wpdb->prepare($count_sql, $where_args);
+        }
+        $count = $wpdb->get_var($count_query);
         return [
             'data' => $window,
-            'count' => $count->count
+            'count' => $count
         ];
     }
 }
